f4556056787e4c386d64bac1d4118f19c9411a4c
Julio Ramirez <julio1031.jr@gmail.com>
2022-06-01 20:56:49 -0700

    have a decent parse rn. will rework it in the future for sure.
    I wanted to change project to module instead of commonjs, then swap off puppeteer and create a clear pipeline for each layer of the directory im scraping. Perhaps save html files so that i dont have to wait for browsers.

--- get_exercise_data.ts
+++ get_exercise_data.ts
@@ -32,14 +32,18 @@ async function Main() {
   for (const muscleGroup in json) {
     for (const muscle_name in json[muscleGroup].muscles) {
       for (const equipment_name in json[muscleGroup].muscles[muscle_name]) {
-        for await (let exercise of json[muscleGroup].muscles[muscle_name][
+        for (let exercise_idx in json[muscleGroup].muscles[muscle_name][
           equipment_name
         ]) {
-          debugger;
-          console.log(`exercise:${[...exercise]}`);
+          let exercise =
+            json[muscleGroup].muscles[muscle_name][equipment_name][
+              exercise_idx
+            ];
+          // debugger;
+          console.log(`exercise:${(exercise.name, exercise.href)}`);
           const name = exercise.name;
           let href = exercise.href;
-          let exerciseData: Exercise = {
+          let parsedData: Exercise = {
             name: name,
             href: href,
             classification: {},
@@ -53,23 +57,25 @@ async function Main() {
             let page = await (
               await fetch.default(href, {
                 headers: {
-                  "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36",
+                  Cookie:
+                    "_ga=GA1.2.122195374.1654023015; _gid=GA1.2.371181797.1654023015; ezds=ffid%3D1%2Cw%3D1920%2Ch%3D1080; _pbjs_userid_consent_data=3524755945110770; __qca=P0-1803175704-1654023020023; __gads=ID=50d18598d9be51a4:T=1654023816:S=ALNI_MZNN-Oj_aLzskDVPMTaPJorT7zaXw; __gpi=UID=000005fbeccab4b1:T=1654023816:RT=1654064582:S=ALNI_MbO8JEuKFJO3G1LMzTJsBoS_WTbqA; cto_bundle=KGVl3V9ZNlE2QUpnOVJkZ3hNM0Q4Y1hGQ1h1ZFAlMkJLWnlyVkNFQUlRJTJCS0JJbTVMZkFtaTlvd3JPYnglMkZHTnpKZmFzV2NjYVVUQkRIJTJCckI0ME1KWHh4QTFVJTJGM2FEYTZkOHVEbE5aNEJtbmtEWTdQbUhpM2hYbUtvYVV3WEY1cUE3RDdNMmFaM0tjT1h3ZFliZVRUJTJGVHlVdEFuMlElM0QlM0Q; cto_bidid=DdJAvV90bGNUUzFsOWwlMkZsTyUyRnF1T2FDY2dsbERadDZTdXlrejVCY3BDUm1OdEM4TWR3UTlwd3czQjlOSlFjM092ZDVGRXFnJTJCWUhyMTd4eHc4bGxtdmlRVDJBZ3F4UmFKQmdRaXEyTER6TW5KUGpiZyUzRA; ezohw=w%3D1764%2Ch%3D269; ezepvv=401; ezosuibasgeneris-1=0ad9a599-ab57-47e3-5cec-a6a52261a87c; active_template::107151=pub_site.1654067241; ezovuuidtime_107151=1654067243; ezux_lpl_107151=1654067305144|8db9c145-d6f0-4a6d-5f0f-5cfc5fce31e3|false",
                   Referer: "https://exrx.net/",
                 },
               })
             ).text();
             if (!page) {
               console.log(`Trouble grabbing html from ${href}`);
-              exercise = exerciseData;
+              json[muscleGroup].muscles[muscle_name][equipment_name][
+                exercise_idx
+              ] = parsedData;
               continue;
             }
             let document = parse(page);
             let infoSections = {};
             const headers = document.querySelectorAll(".ad-banner-block h2");
-            debugger;
+            // debugger;
+
             for (const h2 of headers) {
-              console.log(h2.innerText.toLowerCase().trim());
               infoSections[h2.innerText.toLowerCase().trim()] = {};
               infoSections[h2.innerText.toLowerCase().trim()]["htmlElement"] =
                 h2;
@@ -78,24 +84,25 @@ async function Main() {
             for (const section_name in infoSections) {
               const element = infoSections[section_name]["htmlElement"];
               let res: any;
+              console.log(`${section_name}:`);
               switch (section_name) {
                 case "instructions":
                   res = parseInstructions(element);
                   console.log(res);
-                  exerciseData.instructions = res;
+                  parsedData.instructions = res;
                   break;
                 case "classification":
                   res = parseClassification(element);
                   console.log(res);
-                  exerciseData.classification = res;
+                  parsedData.classification = res;
                   break;
                 case "muscles":
                   res = parseMuscles(element);
                   console.log(res);
-                  exerciseData.muscles = res;
+                  parsedData.muscles = res;
                   break;
                 case "comments":
-                  exerciseData.comments =
+                  parsedData.comments =
                     infoSections[
                       section_name
                     ].htmlElement.nextElementSibling.textContent;
@@ -104,16 +111,17 @@ async function Main() {
                   break;
               }
             }
-            debugger;
+            // debugger;
             let url = findVimeoUrl(page);
             let mp4 = await getMp4(url);
-            exerciseData.vimeoUrl = url;
-            exerciseData.mp4 = mp4;
+            parsedData.vimeoUrl = url;
+            parsedData.mp4 = mp4;
           } else {
             console.log("No href found, skipping exercise");
           }
-          console.log(exerciseData);
-          exercise = exerciseData;
+          console.log(parsedData);
+          json[muscleGroup].muscles[muscle_name][equipment_name][exercise_idx] =
+            parsedData;
         }
       }
     }
@@ -141,7 +149,7 @@ function parseInstructions(htmlElement: any): Exercise["instructions"] {
     }
     return res;
   };
-  debugger;
+  // debugger;
   const p_tags = getnextP(htmlElement.nextElementSibling, []);
   p_tags[0] == "preparation" ? (instructions.preparation = p_tags[1]) : {};
   p_tags[2] == "execution" ? (instructions.execution = p_tags[3]) : {};
@@ -175,7 +183,7 @@ function parseMuscles(htmlElement: any): Exercise["muscles"] {
     }
     return res;
   };
-  debugger;
+  // debugger;
   const items = cycleSiblings(targetElement, []);
   for (const idx in items) {
     switch (items[idx]) {
@@ -198,12 +206,11 @@ function parseClassification(htmlElement: any): Exercise["classification"] {
     mechanics: undefined,
     utility: undefined,
   };
-  debugger;
+  // debugger;
   const tableRows = targetElement.parentNode.querySelectorAll("table tr");
   for (const tr in tableRows) {
     const text = tableRows[tr].textContent.toLowerCase().trim();
     const strings = text.split(":");
-    debugger;
     if (classification[strings[0]] === undefined) {
       classification[strings[0].trim()] = strings[1].trim();
     }
@@ -214,7 +221,6 @@ async function getMp4(url): Promise<any> {
   if (!url) return undefined;
   var config = {
     method: "get",
-    // url: "https://player.vimeo.com/video/156565479?muted=1&autoplay=1&loop=1&title=0&byline=0&portrait=0",
     headers: {
       Cookie:
         'vuid=pl611761060.2142071653; player=""; __cf_bm=h_SUYmE1jK2NumS3_Sj3Lo1HHLvE9lYYFE1w0ZnAgss-1654026332-0-AcPQKPNAi6gP6rR1/i5DpvuEIzVCaPLDbVQm7QEgvRWYuQHChWEl63GqShV+dc0bw+sdRACAul6neZvzWLhVSec=; __cf_bm=4Br2yvmrLxJFFYjGM4geHOhwp54NIsPNWuNZuTJyxrA-1654030279-0-AWMse5rVgWD3MAeK+asiGOllHnJkk+jxe5XHyu802nVsHDZ6yO+vANJ6FOz+99RW6wbanHm1wjRPARPpJvjyz34=',
@@ -223,13 +229,13 @@ async function getMp4(url): Promise<any> {
       Connection: "close",
     },
   };
-  debugger;
+  // debugger;
 
-  const page = await (await fetch.default(url)).text();
-  let targetText = /"progressive":\[\{.*\},*\]/.exec(page);
+  const page = await (await fetch.default(url, config)).text();
+  let targetText = /"progressive":\[\{[^\]]*\},*\]/.exec(page);
   // const document = parse(page);
-  debugger;
-  if (!targetText[0]) return;
+  // debugger;
+  if (!targetText[0]) return undefined;
 
   let cdnObject = JSON.parse(targetText[0].replace('"progressive":', ""));
   let mp4url = cdnObject.pop().url;
@@ -240,16 +246,18 @@ async function getMp4(url): Promise<any> {
 //   await writeFile("output_2.json", JSON.stringify(exercise_data));
 (async () => {
   const outData = await Main();
-  writeFileSync("output_2.json", JSON.stringify(outData));
+  writeFileSync(`${process.cwd()}\\output_2.json`, JSON.stringify(outData));
 })();
 function findVimeoUrl(docText: string): string | undefined {
   const urls = /<iframe.*src=.*vimeo.*<\/iframe>/.exec(docText);
-  if (urls.length > 1) {
-    debugger;
+  if (urls?.length > 1 || !urls) {
+    // debugger;
+    console.log("No vimeo url found");
+    return undefined;
   }
   // console.log(urls);
   const src = urls[0]?.split('src="')[1].split('"')[0] || undefined;
   console.log(src);
-  debugger;
+  // debugger;
   return src;
 }
